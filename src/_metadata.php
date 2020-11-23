973c7cd9537ff899b63fa90e9e5ffb474af5d815
Javi Ramírez <javi.rmrz@gmail.com>
2020-11-23 13:36:27 +0100

    Add og images and favicon

---
+++ src/_metadata.php
@@ -0,0 +1,37 @@
+<meta name="description"
+  content="Is success the result or hard work, skill and dedication – or is it just luck? Play and find out.">
+<meta name="keywords" content="success,hard work,luck,game,play,dedication,skill,hard work vs luck">
+
+<!-- Open Graph data -->
+
+<meta property="og:url" content="https://rameerez.com/success-hard-work-vs-luck/">
+<meta property="og:title" content="Success: is it really hard work or just luck? | Rameerez">
+<meta property="og:description"
+  content="Is success the result or hard work, skill and dedication – or is it just luck? Play and find out.">
+
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://rameerez.com/success-hard-work-vs-luck/images/hard-work-vs-skill-og-image.jpg">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:site_name" content="Hustl">
+<meta property="og:locale" content="en_US">
+
+<!-- Twitter card -->
+<meta name="twitter:card" content="summary_large_image">
+<!-- <meta name="twitter:site" content="@wakefy_app"> -->
+<meta name="twitter:title" content="Success: is it really hard work or just luck? | Rameerez">
+<meta name="twitter:description"
+  content="Is success the result or hard work, skill and dedication – or is it just luck? Play and find out.">
+<meta name="twitter:image" content="https://rameerez.com/success-hard-work-vs-luck/images/hard-work-vs-skill-og-image.jpg">
+
+<!-- Favicons -->
+<link rel="apple-touch-icon" sizes="152x152" href="/images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+<link rel="manifest" href="/images/site.webmanifest">
+<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/images/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="/images/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+
