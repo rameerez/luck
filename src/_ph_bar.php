bf31dfd763a2f0c41bc72cf08d368d495f9804ee
Javi Ramírez <javi.rmrz@gmail.com>
2020-12-01 18:25:06 +0100

    Add PH bar

---
+++ src/_ph_bar.php
@@ -0,0 +1,27 @@
+<style>
+  #ph {
+    display: none;
+    width: 100%;
+    padding: 7px 10px 10px 10px;
+    box-sizing: border-box;
+    background-color: #ea532a;
+    color: rgba(255, 255, 255, 0.9);
+    font-size: 16px;
+    letter-spacing: 0.2px;
+    text-align: center;
+  }
+
+  #ph a {
+    text-decoration: underline;
+  }
+</style>
+
+
+<div id="ph" class="container-fluid">
+  <div class="row">
+    <div class="col text-center">
+      <span id="product-hunt-text">We’re live on Product Hunt! 😻 <a
+          href="https://www.producthunt.com/posts/hustl-2">Come check us out!</a> 🚀</span>
+    </div>
+  </div>
+</div>
