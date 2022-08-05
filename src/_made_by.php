810215b6b296373f76bed64d863c870a63505b50
Javi Ramírez <4920956+rameerez@users.noreply.github.com>
2022-08-05 21:08:33 +0200

    Remove unused dependencies

--- src/_made_by.php
+++ src/_made_by.php
@@ -1,4 +1,4 @@
-<style>
+<!-- <style>
   #made-by-wrapper {
     position: fixed;
     bottom: 0;
@@ -45,4 +45,12 @@
     <a href="https://twitter.com/rameerez">by @rameerez</a>
     <br />
   </div>
-</div>
+</div> -->
+
+
+<!-- Signature -->
+<footer id="signature-made-by" style="position: fixed; bottom:0; right: 0;">
+  <img src="https://rameerez.com/signature/avatar.webp" width="21" style="vertical-align: text-top;">
+  <a href="https://twitter.com/intent/follow?screen_name=rameerez" style="text-decoration: none;">by @rameerez</a>
+</footer> <script async src="https://rameerez.com/signature/signature.min.js" charset="utf-8"></script>
+<!-- End signature -->
