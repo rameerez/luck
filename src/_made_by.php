3fb814f62b80e0e2f59830e30596a692c3de7d6f
Javi Ramírez <4920956+rameerez@users.noreply.github.com>
2022-08-08 16:27:00 +0200

    Update signature

--- src/_made_by.php
+++ src/_made_by.php
@@ -1,56 +1,7 @@
-<!-- <style>
-  #made-by-wrapper {
-    position: fixed;
-    bottom: 0;
-    right: 0;
-    font-size: 14px;
-  }
-
-  #made-by {
-    padding: 1em 2em;
-    padding: 0.5em;
-    border-top-left-radius: 3px;
-    /* border-bottom-left-radius: 3px; */
-    border-top: 1px solid rgba(0, 0, 0, 0.1);
-    background: rgba(255, 255, 255, 0.8);
-    font-weight: 800;
-    border-left: 1px solid rgba(0, 0, 0, 0.1);
-  }
-
-  #made-by,
-  #made-by a {
-    color: rgba(0, 0, 0, 0.6);
-  }
-
-  #made-by img {
-    margin-right: 0.5em;
-    border-radius: 2em;
-    width: 1.5em;
-    vertical-align: middle;
-  }
-
-  #buy-me-a-coffee{
-    margin: 0.5em 0;
-    text-align: center;
-  }
-
-</style>
-
-<div id="made-by-wrapper">
-  <div id="buy-me-a-coffee">
-    <a href="http://buymeacoffee.com/rameerez">☕️ Buy me a coffee</a>
-  </div>
-  <div id="made-by">
-    <img src="https://goedit.me/assets/javi-ramirez-rameerez.jpg">
-    <a href="https://twitter.com/rameerez">by @rameerez</a>
-    <br />
-  </div>
-</div> -->
-
-
 <!-- Signature -->
-<footer id="signature-made-by" style="position: fixed; bottom:0; right: 0;">
-  <img src="https://rameerez.com/signature/avatar.webp" width="21" style="vertical-align: text-top;">
+<footer id="signature-made-by" style="position: fixed; bottom:0; right: 0; font-family: -apple-system, Inter, 'Helvetica', Arial, sans-serif;"
+  allowpopup displaypopupafter="60" displaypopupwhenscrolled="2000" displaypopuponexit="false">
+  <img src="https://rameerez.com/signature/avatar.webp" width="21" height="21" style="vertical-align: text-top;">
   <a href="https://twitter.com/intent/follow?screen_name=rameerez" style="text-decoration: none;">by @rameerez</a>
 </footer> <script async src="https://rameerez.com/signature/signature.min.js" charset="utf-8"></script>
 <!-- End signature -->
